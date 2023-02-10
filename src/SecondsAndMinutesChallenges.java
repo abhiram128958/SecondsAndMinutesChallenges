cc64278d624b99121331f052228ffc3a9d5fbb8e
Abhiram <abhiram.bejavada@gmail.com>
2023-02-10 22:57:07 +0530

    change

--- src/SecondsAndMinutesChallenges.java
+++ src/SecondsAndMinutesChallenges.java
@@ -1,20 +1,29 @@
-public class Main {
+public class SecondsAndMinutesChallenges {
     public static void main(String[] args) {
-        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(-3945));
+        System.out.println(getDurationString(-65,45));
+        System.out.println(getDurationString(65,145));
         System.out.println(getDurationString(65,45));
+        System.out.println(getDurationString(3945));
     }
     public static String getDurationString(int seconds){
-        int minutes = seconds/60;
-        return getDurationString(minutes, seconds);
+        if(seconds<0){
+            return "Invalid data for seconds(" + seconds + "), must be a positive integer value";
+        }
+        return getDurationString(seconds/60, seconds%60);
 
     }
     public static String getDurationString(int minutes, int seconds){
+        if(minutes<0){
+            return "Invalid data for minutes(" + minutes + "), must be a positive integer value";
+        }
+        if(seconds<=0||seconds>=59){
+            return "Invalid data for seconds(" + seconds + "), must be between 0 and 59";
+        }
         int hours = minutes /60;
-//        int hours1=seconds/3600;
-//        System.out.println("hours1 = "+hours1);
+
         int remainingMinutes = minutes%60;
 
-        int remainingSeconds = seconds%60;
-        return hours + "h " + remainingMinutes + "m " + remainingSeconds + "s";
+        return hours + "h " + remainingMinutes + "m " + seconds + "s";
     }
 }
